55be3e6c0038183201af2953c68096ffa746f154
Kelly de Vries <kdv@me.com>
2015-11-15 21:17:01 -0800

    add api request

--- SearchPage.js
+++ SearchPage.js
@@ -82,19 +82,32 @@ class SearchPage extends Component {
     super(props);
     this.state = {
       searchString: 'london',
-      isLoading: false
+      isLoading: false,
+      message: ''
     };
   }
   onSearchTextChanged(event) {
-    console.log('onSearchTextChanged');
     this.setState({ searchString: event.nativeEvent.text });
-    console.log(this.state.searchString);
   }
   _executeQuery(query) {
-  console.log(query);
   this.setState({ isLoading: true });
+  fetch(query)
+    .then(response => response.json())
+    .then(json => this._handleResponse(json.response))
+    .catch(error =>
+       this.setState({
+        isLoading: false,
+        message: 'Something bad happened ' + error
+     }));
   }
-
+  _handleResponse(response) {
+    this.setState({ isLoading: false , message: '' });
+    if (response.application_response_code.substr(0, 1) === '1') {
+      console.log('Properties found: ' + response.listings.length);
+    } else {
+      this.setState({ message: 'Location not recognized; please try again.'});
+    }
+  }
   onSearchPressed() {
     var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
     this._executeQuery(query);
@@ -132,6 +145,7 @@ class SearchPage extends Component {
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
         {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
